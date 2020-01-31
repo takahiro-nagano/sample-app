39c2b45fa13d64e4a19bee37869d594cd7710c74
EC2 Default User <ec2-user@ip-172-31-45-173.ap-northeast-1.compute.internal>
2020-01-31 07:25:35 +0000

    1月31日②、更新機能ができなくて苦戦中

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
   
+  
+  
   def show
     @user = User.find(params[:id])
   end
@@ -31,9 +33,20 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
     
+  def update_basic
+      @user = User.find(params[:id])
+   if @user.update_attributes(basic_params)
+      flash[:success] = "#{@user.name}の基本情報を更新しました。"
+   else
+      flash[:danger] = "#{@user.name}の更新は失敗しました。<br>" + @user.errors.full_messages.join("<br>")
+   end
+      redirect_to users_url
+  end
+    
+    
   def update
      @user = User.find(params[:id])
-   if @user.update(user_params)
+   if @user.update_attributes(user_params)
      redirect_to user_path
    else
      render 'edit'
@@ -47,7 +60,13 @@ class UsersController < ApplicationController
   
   private
   
-    def user_params
-      params.require(:user).permit(:name, :email, :department, :password, :password_confirmation)
-    end
+  def user_params
+    params.require(:user).permit(:name, :email, :department, :password, :password_confirmation)
+  end
+    
+  def basic_params
+    params.require(:user).permit(:basic_time, :work_time)
+  end  
+    
+    
 end
