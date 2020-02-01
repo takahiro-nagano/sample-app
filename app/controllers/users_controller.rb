c18191afb706f960dfd7349364e281e991b2b1b0
EC2 Default User <ec2-user@ip-172-31-45-173.ap-northeast-1.compute.internal>
2020-02-01 07:00:15 +0000

    2月1日更新処理がやっとできたところ

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,6 @@
 class UsersController < ApplicationController
   
 before_action :set_user, only: [:show, :edit, :update, :destroy, :edit_basic_info, :update_basic_info]
-  before_action :logged_in_user, only: [:index, :edit, :update, :destroy, :edit_basic_info, :update_basic_info]
-  before_action :correct_user, only: [:edit, :update]
   before_action :admin_user, only: [:destroy, :edit_basic_info, :update_basic_info]
   before_action :set_one_month, only: :show
 
@@ -46,7 +44,7 @@ before_action :set_user, only: [:show, :edit, :update, :destroy, :edit_basic_inf
    if @user.update_attributes(basic_params)
       flash[:success] = "#{@user.name}の基本情報を更新しました。"
    else
-      flash[:danger] = "#{@user.name}の更新は失敗しました。<br>" + @user.errors.full_messages.join("<br>")
+      flash[:danger] = "#{@user.name}の更新は失敗しとうよ。<br>" + @user.errors.full_messages.join("<br>")
    end
       redirect_to users_url
   end
